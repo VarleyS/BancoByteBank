5ab9f3e15f2384ad85d494593cfca5ed10646859
VarleyS <varleysr@gmail.com>
2022-07-12 19:38:10 -0300

    Adicionar arquivos de projeto.

---
+++ BancoByteBank/ContaCorrente.cs
@@ -0,0 +1,9 @@
+﻿
+
+public class ContaCorrente
+{
+    public string Titular;
+    public int Agencia;
+    public int Numero;
+    public double Saldo;
+}
---
+++ BancoByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BancoByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaCorrente = new ContaCorrente();
+
+            contaCorrente.Titular = "Gabriela";
+            contaCorrente.Agencia = 864;
+            contaCorrente.Numero = 214545;
+            contaCorrente.Saldo = 100;
+
+
+
+            Console.WriteLine("Nome do titular: " + contaCorrente.Titular);
+            Console.WriteLine("Agencia: " + contaCorrente.Agencia);
+            Console.WriteLine("Número: " + contaCorrente.Numero);
+            Console.WriteLine("Saldo: " + contaCorrente.Saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
